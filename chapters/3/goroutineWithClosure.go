36c84365aa904907bb263eacdfd3d8f32be81167
kitakou0313 <kitakou0313@gmail.com>
2025-02-14 08:03:39 +0900

    add: add sample of go routine with loop

--- chapters/3/goroutineWithClosure.go
+++ chapters/3/goroutineWithClosure.go
@@ -8,12 +8,22 @@ import (
 func main() {
 	var wg sync.WaitGroup
 
-	salutaion := "Hello"
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		salutaion = "Hello in goroutine"
-	}()
+	// salutaion := "Hello"
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	salutaion = "Hello in goroutine"
+	// }()
+	// wg.Wait()
+	// fmt.Println(salutaion)
+
+	for _, salutaion := range []string{"Hello", "Greetings", "Good day"} {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			fmt.Println(salutaion)
+		}()
+	}
 	wg.Wait()
-	fmt.Println(salutaion)
 }
