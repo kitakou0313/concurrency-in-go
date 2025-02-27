862cd5f09c2702a880d65823eb6fcfe9f9e64a28
kitakou0313 <kitakou0313@gmail.com>
2025-02-28 05:57:44 +0900

    add: add sample of livelock

--- chapters/1/liveLock.go
+++ chapters/1/liveLock.go
@@ -39,4 +39,36 @@ func main() {
 		atomic.AddInt32(dir, -1)
 		return false
 	}
+	var left, right int32
+	tryLeft := func(out *bytes.Buffer) bool {
+		return tryDir("left", &left, out)
+	}
+	tryRight := func(out *bytes.Buffer) bool {
+		return tryDir("right", &right, out)
+	}
+
+	walk := func(walking *sync.WaitGroup, name string) {
+		var out bytes.Buffer
+		defer func() {
+			fmt.Println(out.String())
+		}()
+		defer walking.Done()
+
+		fmt.Fprintf(&out, "%v is trying to scoot:", name)
+
+		for i := 0; i < 5; i++ {
+			if tryLeft(&out) || tryRight(&out) {
+				return
+			}
+		}
+		fmt.Fprintf(&out, "\n%v tosse her hands up in exasperation", name)
+	}
+
+	var peopleInHallway sync.WaitGroup
+
+	peopleInHallway.Add(2)
+	go walk(&peopleInHallway, "Alice")
+	go walk(&peopleInHallway, "Bob")
+	peopleInHallway.Wait()
+
 }
