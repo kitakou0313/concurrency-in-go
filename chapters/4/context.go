a1abc34d19accff7604bc0908d0e3a6e20a483c0
kitakou0313 <kitakou0313@gmail.com>
2025-03-11 07:56:02 +0900

    add: add sample of context

--- chapters/4/context.go
+++ chapters/4/context.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
 )
 
@@ -27,10 +29,59 @@ func genGreeting(ctx context.Context) (string, error) {
 	}
 }
 
+func genFarewell(ctx context.Context) (string, error) {
+	switch locale, err := locale(ctx); {
+	case err != nil:
+		return "", err
+	case locale == "EN/US":
+		return "Goodbye", nil
+	}
+
+	return "", fmt.Errorf("unsupported locale")
+}
+
 func printGreeting(ctx context.Context) error {
-	greeting, err := gre
+	greeting, err := genGreeting(ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s, world!_n", greeting)
+	return nil
+}
+
+func printFarewell(ctx context.Context) error {
+	farewell, err := genFarewell(ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s, world!_n", farewell)
+	return nil
 }
 
 func main() {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		if err := printGreeting(ctx); err != nil {
+			fmt.Printf("error :%v\n", err)
+			cancel()
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		if err := printFarewell(ctx); err != nil {
+			fmt.Printf("error :%v\n", err)
+			cancel()
+		}
+	}()
+
+	wg.Wait()
 }
